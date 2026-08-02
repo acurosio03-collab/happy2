e02852ff12b8f011460e77e5f23c86ae4a884b5f
acurosio03-collab <acurosio03@gmail.com>
2026-08-02 15:41:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,4 @@
 // ===============================
-// COUNTDOWN ULANG TAHUN
-// ===============================
-
-// Atur tanggal ulang tahun
-let targetDate = new Date("September 28, 2026 00:00:00").getTime();
-
-
-let countdown = setInterval(function(){
-
-    let now = new Date().getTime();
-
-    let distance = targetDate - now;
-
-
-    let hari = Math.floor(
-        distance / (1000 * 60 * 60 * 24)
-    );
-
-    let jam = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) /
-        (1000 * 60 * 60)
-    );
-
-    let menit = Math.floor(
-        (distance % (1000 * 60 * 60)) /
-        (1000 * 60)
-    );
-
-    let detik = Math.floor(
-        (distance % (1000 * 60)) /
-        1000
-    );
-
-
-    document.getElementById("timer").innerHTML =
-    hari + " Hari " +
-    jam + " Jam " +
-    menit + " Menit " +
-    detik + " Detik";
-
-
-    // Jika waktu sudah tiba
-    if(distance < 0){
-
-        clearInterval(countdown);
-
-        document.getElementById("timer").innerHTML =
-        "🎉 HAPPY BIRTHDAY 🎉";
-
-    }
-
-
-},1000);
-
-
-
-// ===============================
-// BUKA KADO
-// ===============================
-
-// ===============================
 // TANGGAL HARI H
 // ===============================
 
@@ -103,23 +42,6 @@ function openGift(){
         music.play().catch(() => {});
     }
 
-    alert("🎉 Selamat Ulang Tahun! Semoga selalu bahagia ❤️");
-}
-    
-
-    document.getElementById("gift").style.display="block";
-
-
-    alert("🎁 Kejutan dibuka! Selamat ulang tahun sayang semoga panjang umurnya berbakti pada orang tua lancar terus rezeki jaga kesehatan nya okeii❤️");
-
-}
-// ===============================
-// MUSIK ULANG TAHUN
-// ===============================
-function playMusic(){
-
-    let music = document.getElementById("music");
-
-    music.play();
+    alert("🎉 Kejutan dibuka! Selamat ulang tahun sayang semoga panjang umurnya berbakti pada orang tua lancar terus rezeki jaga kesehatan nya okeii❤️");
 
 }
